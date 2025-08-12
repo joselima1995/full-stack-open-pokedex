9b3e6f702847d59af688e271826780063f02f2f6
fc51790 <fc51790@alunos.fc.ul.pt>
2025-08-12 17:17:48 +0100

    e2e testing

---
+++ playwright.config.js
@@ -0,0 +1,16 @@
+/** @type {import('@playwright/test').PlaywrightTestConfig} */
+const config = {
+  testDir: './e2e-tests',   // Only run tests from here
+  testMatch: '**/*.spec.js', // Or .ts, whatever your E2E tests use
+  webServer: {
+    command: 'npm run start',
+    url: 'http://localhost:5000',
+    timeout: 120 * 1000,
+  },
+  timeout: 120 * 1000,
+  use: {
+    baseURL: 'http://localhost:5000/',
+  },
+}
+
+module.exports = config
