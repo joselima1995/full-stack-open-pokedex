e3929535604aff7fd060bc252cd31636618125c6
fc51790 <fc51790@alunos.fc.ul.pt>
2025-08-13 13:09:09 +0100

    fixing script

--- build_step.sh
+++ build_step.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-echo "Running custom build step..."
+echo "Running build step..."
 
-# Install dependencies for client and build it
-cd client
 npm install
 npm run build
-cd ..
-
-# Install dependencies for server
-cd server
-npm install
-cd ..
