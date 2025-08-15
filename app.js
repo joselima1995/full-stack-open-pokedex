eda6aea3dd29d117b6e3272a879b1e44ea118633
fc51790 <fc51790@alunos.fc.ul.pt>
2025-08-15 21:40:39 +0100

    Automatic deployments health check fail

--- app.js
+++ app.js
@@ -12,10 +12,12 @@ app.listen(PORT, () => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('2')
+  res.send('3')
 })
 
 
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
   res.send('ok')
 })
